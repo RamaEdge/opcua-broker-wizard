561bfc436d8080a0dce32bcf003fb61513b2f58b
Ravi Chillerega <chilleregeravi@gmail.com>
2025-03-15 18:29:04 +0000

    Updated linting

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -1,8 +1,9 @@
-import * as React from "react"
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
+import * as React from "react"
 
+import type { ButtonProps } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
-import { ButtonProps, buttonVariants } from "@/components/ui/button"
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
@@ -115,3 +116,5 @@ export {
   PaginationNext,
   PaginationPrevious,
 }
+
+export { buttonVariants }
