3b8d0fa94c95cf232cf6af4ca0793847179906be
Ravi Chillerega <chilleregeravi@gmail.com>
2025-03-15 10:20:02 +0000

    Fixed issue with build

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
+///** @type {import('postcss').Config} */
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
 }
--- tailwind.config.ts
+++
@@ -1,5 +0,0 @@
-
-import type { Config } from "tailwindcss";
-import tailwindConfig from "./src/styles/tailwind";
-
-export default tailwindConfig as Config;
